8d2f77ecad0ccaeecac317a1f1f435726cf56371
phone clinc <ithouph@gmail.com>
2025-11-12 09:51:39 -0500

    trying ta push my style

--- App.js
+++ App.js
@@ -1,7 +1,15 @@
 import React, { useState } from "react";
 import { View } from "react-native";
-import SignIn from "./auth/signin";
-import SignUp from "./auth/signup";
+
+import SignIn from "./auth/Signin";
+import SignUp from "./auth/Signup";
+// import from "./routers/";
+import Discover from "./router/Discover";
+import Explore from "./router/Explore";
+import Saved from "./router/Saved";
+import Profile from "./router/Profile";
+
+
 import { useTheme } from "./theme/useTheme";
 
 export default function App() {
@@ -21,5 +29,152 @@ export default function App() {
 
 
 
-// ?>S?FD/
-// ?>S?FD//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import Detail from "./pages/Detail";
+
+
+import NavBar from "./components/Navbar";
+
+import { useTheme } from "./theme/global";
+
+export default function App() {
+  const [page, setPage] = useState("signIn"); // "signIn", "signUp", or "Dashboard"
+  const { colors } = useTheme();
+
+  return (
+    <View style={{ flex: 1, backgroundColor: colors.background }}>
+      {page === "signIn" && (
+        <SignIn
+          goToSignUp={() => setPage("signUp")}
+          goToDiscover={() => setPage("Discover")}
+        />
+      )}
+
+      {page === "signUp" && (
+        <SignUp
+          goToSignIn={() => setPage("signIn")}
+          goToDiscover={() => setPage("Discover")}
+        />
+      )}
+      {page === "Discover" && (
+        <View style={{ flex: 1 }}>
+          <Discover />
+          <NavBar page={page} setPage={setPage} />
+        </View>
+      )}
+
+      {page === "Explore" && (
+        <View style={{ flex: 1 }}>
+          <Explore />
+          <NavBar page={page} setPage={setPage} />
+        </View>
+      )}
+      {page === "Saved" && (
+        <View style={{ flex: 1 }}>
+          <Saved />
+          <NavBar page={page} setPage={setPage} />
+        </View>
+      )}
+      {page === "Profile" && (
+        <View style={{ flex: 1 }}>
+          <Profile />
+          <NavBar page={page} setPage={setPage} />
+        </View>
+      )}
+      {page === "Detail" && (
+        <View style={{ flex: 1 }}>
+          <Detail />
+          <NavBar page={page} setPage={setPage} />
+        </View>
+      )}
+
+
+    </View>
+  );
+}
